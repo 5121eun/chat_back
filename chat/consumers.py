7e7cf89461a02067f641e29dade3c4ab66df9111
5121eun <5121eun@gmail.com>
2024-04-02 23:24:30 +0900

    update t5 model

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,8 +2,16 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base", legacy=True)
+        self.model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+
     async def connect(self):
         await self.accept()
 
@@ -13,6 +21,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        input_ids = self.tokenizer(message, return_tensors="pt").input_ids
+
+        outputs = self.model.generate(input_ids)
+        response = self.tokenizer.decode(outputs[0]).replace('<pad>', '').replace('</s>', '')
 
         await self.send(text_data=json.dumps([
             {
@@ -21,6 +33,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             },
             {
                 "type": False,
-                "value": f"from server: {message}"
+                "value": response
             }
         ]))
